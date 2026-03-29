52bf3b2395f787b8fb9df02354f42c606d5b87b4
Julian Liu <jul14nl1u@gmail.com>
2026-03-28 21:42:55 -0400

    feat: for PWA installability, add manifest & icons, modify meta tags

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,54 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'UploadSoul',
+        short_name: 'UploadSoul',
+        id: '/',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        theme_color: '#156b5c',
+        background_color: '#212223',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        screenshots: [
+          {
+            src: "/screenshot-mobile.png",
+            sizes: "360x720",
+            type: "image/png"
+          },
+          {
+            src: "/screenshot-desktop.png",
+            sizes: "1280x720",
+            type: "image/png",
+            form_factor: "wide"
+          }
+        ]
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4000000
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
